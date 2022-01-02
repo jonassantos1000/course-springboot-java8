cb5fd073397e7d4b2a8a896b1201dd3a797664da
jonassantos1000 <jonasvale3@hotmail.com>
2022-01-02 02:38:13 -0300

    User entity and resource

---
+++ src/main/java/com/conatus/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.conatus.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.conatus.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Jonas","jonas@vale3@hotmail.com","9999999","12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
