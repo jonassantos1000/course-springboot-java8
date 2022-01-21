a5376e6d9264b8bf6af6902f10c42f01c0827f53
jonassantos1000 <jonasvale3@hotmail.com>
2022-01-21 00:41:35 -0300

    Many-to-Many association with JoinTable

--- src/main/java/com/conatus/course/entities/Product.java
+++ src/main/java/com/conatus/course/entities/Product.java
@@ -8,27 +8,30 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
-@Table(name="tb_product")
+@Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-		
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //Com isso o Banco gera o codigo de cliente automaticamente.
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Com isso o Banco gera o codigo de cliente automaticamente.
 	private Long id;
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product() {
-		
+
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -108,6 +111,5 @@ public class Product implements Serializable {
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
-	
+
 }
