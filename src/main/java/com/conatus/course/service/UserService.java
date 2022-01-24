3afd7640073527fbd90ad4bf3c8d683ce593df2c
jonassantos1000 <jonasvale3@hotmail.com>
2022-01-24 16:01:49 -0300

    Exception handling - findById

--- src/main/java/com/conatus/course/service/UserService.java
+++ src/main/java/com/conatus/course/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.conatus.course.entities.User;
 import com.conatus.course.repositories.UserRepository;
+import com.conatus.course.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User FindById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert (User obj) {
